3a3d9b43b228a490f13cf343fc0b3ef332e39c43
Gustavo <gustavodacorte@gmail.com>
2020-11-26 22:23:54 +0900

    minor bugs fixed

--- public/index.php
+++ public/index.php
@@ -7,12 +7,20 @@
  */
 // 
 
-session_start();
+if(!isset($_SESSION)) 
+{ 
+    session_start(); 
+} 
+
+if(!isset($_SERVER['REDIRECT_URL'])){
+    $_SERVER['REDIRECT_URL'] = "/";
+}
+
 /**
  * The webserver should point to this public folder as the document_root of the host. 
  * All incoming requests are caught by the  .htaccess file and directed here. Here the router will map the route to the appropriate controller/action
  */
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . '..'. DIRECTORY_SEPARATOR . 'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
 
 Use Routes\Router;
 
